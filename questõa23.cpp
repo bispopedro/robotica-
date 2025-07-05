76fbe3d6324652adf4e24f42ac947ce187655538
bispopedroiago <pedroiagolimalimalima0203@gmail.com>
2025-07-05 18:29:31 -0300

    Update questõa23.cpp

--- questõa23.cpp
+++ questõa23.cpp
@@ -1,25 +1,7 @@
-
+#pragma once
 #include <iostream>
 using namespace std;
 
 void mostrarInfo() {
     cout << "Informacoes do programa." << endl;
 }
-
-
-#include <iostream>
-using namespace std;
-
-float mediaVetor(int* v, int tamanho) {
-    int soma = 0;
-    for (int i = 0; i < tamanho; i++)
-        soma += v[i];
-    return (float)soma / tamanho;
-}
-
-int main() {
-    int v[10] = {1,2,3,4,5,6,7,8,9,10};
-    cout << mediaVetor(v, 10) << endl;
-    return 0;
-}
-
